10f118472f96faa51e54ce47dfad19c65b28f816
pnk2ponkotsu <111572219+pnk2ponkotsu@users.noreply.github.com>
2022-10-26 14:15:03 +0000

    [indexページの作成]3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
   get 'books/new'
-  get 'books/index'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
   get 'books/show'
   get 'books/edit'
  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
 end

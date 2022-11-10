ca8a48f2dcc6cd6f4f2dc3c86cca519872d663e0
pnk2ponkotsu <111572219+pnk2ponkotsu@users.noreply.github.com>
2022-11-10 11:52:47 +0000

    [チェックシートバリデーションまで]3-12

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'book/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
  
  
 end
